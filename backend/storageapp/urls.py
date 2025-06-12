9665b6cb553ef849c66a23e113d356298889edc4
shristy <shristychaudhary4679@gmail.com>
2025-06-12 13:14:00 +0530

    add home and logout views and url paths

--- backend/storageapp/urls.py
+++ backend/storageapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello
+from .views import hello, home, logout_view
 
 urlpatterns = [
     path('hello/', hello),
+    path('', home),
+    path('logout', logout_view)
 ]
--- backend/storageapp/views.py
+++ backend/storageapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,3 +7,10 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def hello(request):
     return Response({"message": "hello from djangooooooo"})
+
+def home(request):
+    return redirect('http://localhost:3000')
+
+def logout_view(request):
+    logout(request)
+    return redirect('http://localhost:3000')
