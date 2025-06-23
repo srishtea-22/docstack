a00f3794580cc1b9aa1a5e8cd71807998685aeb0
shristy <shristychaudhary4679@gmail.com>
2025-06-23 19:54:35 +0530

    Setup Prisma Schema and signup route

---
+++ server/routes/signup.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt";
+import { z } from "zod";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+const userSchema = z.object({
+  username: z.string().min(1, {message: "Username is required"}),
+  email: z.string().email({message: "Invalid email address"}),
+  password: z.string().min(8, {message: "Password must be at least 8 characters"}),
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const {username, email, password} = userSchema.parse(req.body);
+
+        const existingUseremail = await prisma.user.findUnique({
+            where: {email: email}
+        })
+        if (existingUseremail) {
+            return res.status(409).json({
+                user: null,
+                message: "User with this email already exists"
+            })
+        }
+        const existingUsername = await prisma.user.findUnique({
+            where: {username: username}
+        })
+        if (existingUsername) {
+            return res.status(409).json({
+                user: null,
+                message: "User with this username already exists"
+            })
+        }
+
+        const hashedPassword = await hash(password, 10);
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        })
+
+        res.status(201).json({
+            message: "user created successfully",
+            user: newUser
+        })
+    }
+    catch (e){
+        console.error("error while post request", e);
+    }
+})
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/signup.js"
 const app = express();
 app.use(cors());
+app.use(express.json());
 const PORT = 8080;
 
-app.get("/api", (req, res) => {
+app.use("/signup", authRoutes);
+
+app.get("/", (req, res) => {
     res.json({"message": "heyaaaaaa"});
 });
 
